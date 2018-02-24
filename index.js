d471132a25fca8bb877a10a219289c8815504d54
DennisVanBets <dennis.vanbets@gmail.com>
2018-02-24 14:57:05 +0000

    added timeout to read pins

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var gpio = require('rpi-gpio');
 
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://localhost:1883');
+
+var pinRead = false;
+
 console.log('running'); 
 client.on('connect', function () {
   console.log('connected');
@@ -9,13 +12,20 @@ client.on('connect', function () {
 })
  
 gpio.on('change', function(channel, value) {
-    console.log('Channel ' + channel + ' value is now ' + value);
-    if(channel == 11 && value == false) {
-        client.publish('score', 'red');
-    }
-    if(channel == 13 && value == false) {
-        client.publish('score', 'green');
+    if (!pinRead) {
+        console.log('Channel ' + channel + ' value is now ' + value);
+        if(channel == 11 && value == false) {
+            client.publish('score', 'red');
+            console.log('pub score red');
+        }
+        if(channel == 13 && value == false) {
+             client.publish('score', 'green');
+             console.log('pub score green');
+        }
+     pinRead = true;
+     console.log('Pin read, waiting for timeout');
+     setTimeout(function() {pinRead = false; console.log('ready to read');}, 5000);
     }
 });
-gpio.setup(11, gpio.DIR_IN, gpio.EDGE_FALLING);
+gpio.setup(37, gpio.DIR_IN, gpio.EDGE_FALLING);
 gpio.setup(13, gpio.DIR_IN, gpio.EDGE_FALLING);
